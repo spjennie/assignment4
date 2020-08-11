d565b389a7225269a0febe37d095a2f1b6a0b6b6
spjennie <31905943+spjennie@users.noreply.github.com>
2020-08-11 18:21:26 -0400

    Update tutorial2.sql

--- assignment4/tutorial2.sql
+++ assignment4/tutorial2.sql
@@ -50,7 +50,6 @@ SELECT name, capital FROM world
 /* show the name and the capital where the first letters of each match */
 
 SELECT name
-
 FROM world
   WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND    name LIKE '%u%'
   AND name NOT LIKE '% %'
